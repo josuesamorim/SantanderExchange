51473b7fdd0f667d76893d2962e4e97282534382
Josué Amorim <josuedesouzaamorim@gmail.com>
2023-09-28 11:12:00 -0300

    created Stores

---
+++ Santander Exchange/DataProviders/NetworkRepository/Stores/BaseStore.swift
@@ -0,0 +1,56 @@
+//
+//  BaseStore.swift
+//  Santander Exchange
+//
+//  Created by Josué Amorim on 28/09/23.
+//
+
+import Foundation
+
+class BaseStore {
+    
+    let error = NSError(domain: "", code: 901, userInfo: [NSLocalizedDescriptionKey: "Error getting information"]) as Error
+    
+    struct RateResult<Rates: Codable>: Codable {
+        var base: String?
+        var success: Bool = false
+        var rates: Rates?
+        
+        init(data: Data?, response: URLResponse?) throws {
+            guard let data = data, let response = response as? HTTPURLResponse
+            else {
+                throw NSError(domain: "", code: 901, userInfo: [NSLocalizedDescriptionKey: "Error getting information"]) as Error
+            }
+            
+            if let url = response.url?.absoluteString, let json = String(data: data, encoding: .utf8){
+                print("\(response.statusCode): \(url)")
+                print("\(json)")
+            }
+            
+            self = try JSONDecoder().decode(RateResult.self, from: data)
+        }
+    }
+    
+    struct SymbolResult: Codable {
+        
+        var base: String?
+        var success: Bool = false
+        var symbols: CurrencySymbolObject?
+        
+        init(data: Data?, response: URLResponse?) throws {
+            guard let data = data, let response = response as? HTTPURLResponse
+            else {
+                throw NSError(domain: "", code: 901, userInfo: [NSLocalizedDescriptionKey: "Error getting information"]) as Error
+            }
+            
+            if let url = response.url?.absoluteString, let json = String(data: data, encoding: .utf8){
+                print("\(response.statusCode): \(url)")
+                print("\(json)")
+            }
+            
+            self = try JSONDecoder().decode(SymbolResult.self, from: data)
+        }
+        
+    }
+    
+}
