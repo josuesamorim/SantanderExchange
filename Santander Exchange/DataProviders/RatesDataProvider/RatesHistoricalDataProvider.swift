2bb7ec35c825c9f1af69b2ffa6198e65c90fc8ed
Josué Amorim <josuedesouzaamorim@gmail.com>
2023-09-29 07:06:45 -0300

    Created RatesHistoricalDataProvider

--- Santander Exchange/DataProviders/RatesDataProvider/RatesHistoricalDataProvider.swift
+++ Santander Exchange/DataProviders/RatesDataProvider/RatesHistoricalDataProvider.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+protocol RatesHistoricalDataProviderDelegate: DataProviderManagerDelegate {
+    func success(model: RatesHistoricalObject)
+}
+
+class RatesHistoricalDataProvider: DataProviderManager<RatesHistoricalDataProviderDelegate, RatesHistoricalObject>{
+    
+    private let ratesStore: RatesStore
+    
+    init(ratesStore: RatesStore = RatesStore()) {
+        self.ratesStore = ratesStore
+    }
+    
+    func fetchTimesseries(by base: String, from symbols: [String], startDate: String, endDate: String){
+        Task.init {
+            do {
+                let model = try await ratesStore.fetchTimesseries(by: base, from: symbols, startDate: startDate, endDate: endDate)
+                delegate?.success(model: model)
+            } catch {
+                delegate?.errorData(delegate, error: error)
+            }
+        }
+    }
+    
+    
+}
