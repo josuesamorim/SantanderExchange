af1c8ef9e16313e06aef472935d5933ed1d8ce7d
Josué Amorim <josuedesouzaamorim@gmail.com>
2023-09-29 07:07:51 -0300

    created CurrencySymbolsDataProvider

--- Santander Exchange/DataProviders/CurrencySymbolsDataProvider/CurrencySymbolsDataProvider.swift
+++ Santander Exchange/DataProviders/CurrencySymbolsDataProvider/CurrencySymbolsDataProvider.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+protocol CurrencySymbolsDataProviderDelegate: DataProviderManagerDelegate {
+    func success(model: CurrencySymbolObject)
+}
+
+class CurrencySymbolsDataProvider: DataProviderManager<CurrencySymbolsDataProviderDelegate, CurrencySymbolObject>{
+    
+    private let currencyStore: CurrencyStore
+    
+    init(currencyStore: CurrencyStore = CurrencyStore()) {
+        self.currencyStore = currencyStore
+    }
+    
+    func fetchSymbols(){
+        Task.init {
+            do {
+                let model = try await currencyStore.fetchSymbols()
+                delegate?.success(model: model)
+            } catch {
+                delegate?.errorData(delegate, error: error)
+            }
+        }
+    }
+    
+    
+}
