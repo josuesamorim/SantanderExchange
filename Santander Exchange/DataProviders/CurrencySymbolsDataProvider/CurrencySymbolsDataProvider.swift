c7a63c9a0b7c14ac6d0f42ce0725d969d49e2e05
Josué Amorim <josuedesouzaamorim@gmail.com>
2023-09-29 07:06:17 -0300

    Created RatesStore

---
+++ Santander Exchange/DataProviders/CurrencySymbolsDataProvider/CurrencySymbolsDataProvider.swift
@@ -0,0 +1,8 @@
+//
+//  CurrencySymbolsDataProvider.swift
+//  Santander Exchange
+//
+//  Created by Josué Amorim on 29/09/23.
+//
+
+import Foundation
---
+++ Santander Exchange/DataProviders/NetworkRepository/Stores/RatesStore.swift
@@ -0,0 +1,42 @@
+//
+//  RatesStore.swift
+//  Santander Exchange
+//
+//  Created by Josué Amorim on 29/09/23.
+//
+
+import Foundation
+
+protocol RatesStoreProtocol {
+    func fetchFluctuation(by base: String, from symbols: [String], startDate: String, endDate: String) async throws -> RatesFluctuationObject
+    func fetchTimesseries(by base: String, from symbols: [String], startDate: String, endDate: String) async throws -> RatesHistoricalObject
+}
+
+
+class RatesStore: BaseStore, RatesStoreProtocol {
+    func fetchFluctuation(by base: String, from symbols: [String], startDate: String, endDate: String) async throws -> RatesFluctuationObject {
+        guard let urlRequest = try RatesRouter.fluctuation(base: base, symbols: symbols, startDate: startDate, endDate: endDate).asUrlRequest() else {
+            throw error
+        }
+        
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        guard let rates = try RateResult<RatesFluctuationObject>(data: data, response: response).rates else {
+            throw error
+        }
+        return rates
+    }
+    
+    func fetchTimesseries(by base: String, from symbols: [String], startDate: String, endDate: String) async throws -> RatesHistoricalObject {
+        guard let urlRequest = try RatesRouter.timeseries(base: base, symbols: symbols, startDate: startDate, endDate: endDate).asUrlRequest() else {
+            throw error
+        }
+        
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        guard let rates = try RateResult<RatesHistoricalObject>(data: data, response: response).rates else {
+            throw error
+        }
+        return rates
+    }
+    
+    
+}
