81554df8aa3128733d6bba0a024270a82715e182
Josué Amorim <josuedesouzaamorim@gmail.com>
2023-09-29 07:07:16 -0300

    created CurrencyStore

--- Santander Exchange/DataProviders/NetworkRepository/Stores/CurrencyStore.swift
+++ Santander Exchange/DataProviders/NetworkRepository/Stores/CurrencyStore.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+protocol CurrencyStoreProtocol {
+    func fetchSymbols() async throws -> CurrencySymbolObject
+}
+
+class CurrencyStore: BaseStore, CurrencyStoreProtocol {
+    func fetchSymbols() async throws -> CurrencySymbolObject {
+        guard let urlRequest = try CurrencyRouter.symbols.asUrlRequest() else {
+            throw error
+        }
+        
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        guard let symbols = try SymbolResult(data: data, response: response).symbols else {
+            throw error
+        }
+        return symbols
+    }
+}
