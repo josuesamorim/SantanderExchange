fc8700a9b89e183628bacc83c365cf7b851e554e
Josué Amorim <josuedesouzaamorim@gmail.com>
2023-09-29 07:07:34 -0300

    created DataProviderManager

--- Santander Exchange/DataProviders/DataProviderManager.swift
+++ Santander Exchange/DataProviders/DataProviderManager.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+protocol DataProviderManagerDelegate {
+    func success(model: Any)
+    func errorData(_ provider: DataProviderManagerDelegate?, error: Error)
+}
+
+extension DataProviderManagerDelegate {
+    
+    func success(model: Any){
+        preconditionFailure("This method must be overridden")
+    }
+    
+    func errorData(_ provider: DataProviderManagerDelegate?, error: Error){
+        print(error.localizedDescription)
+    }
+}
+
+class DataProviderManager<T, S> {
+    var delegate: T?
+    var model: S?
+}
