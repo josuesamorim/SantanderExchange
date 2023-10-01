90ad9df2fc851c49cd788fd48d4826c743ba6b54
Josué Amorim <josuedesouzaamorim@gmail.com>
2023-10-01 09:45:21 -0300

    Created FluctuationObject and RatesHistoricalObject

--- Santander Exchange/DataProviders/NetworkRepository/Objects/RatesHistoricalObject.swift
+++ Santander Exchange/DataProviders/NetworkRepository/Objects/RatesHistoricalObject.swift
@@ -7,4 +7,27 @@
 
 import Foundation
 
-typealias RatesHistoricalObject = [String: [String : String]]
+
+struct RateObject<Rates: Codable>: Codable {
+    var base: String?
+    var success: Bool = false
+    var rates: Rates?
+}
+
+typealias RatesFluctuationObject = [String: FluctuationObject]
+
+struct FluctuationObject: Identifiable, Codable {
+    
+    let id = UUID()
+    let change: Double
+    let changePct: Double
+    let endRate: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case change
+        case changePct = "change_pct"
+        case endRate = "end_rate"
+    }
+}
+
+typealias RatesHistoricalObject = [String: [String: Double]]
