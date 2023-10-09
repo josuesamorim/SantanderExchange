b907e1b6d98ee62266166b2ddb700fd6676608f6
Josué Amorim <josuedesouzaamorim@gmail.com>
2023-10-09 10:40:52 -0300

    Created ButtonsView

--- Santander Exchange/Views/ButtonsView.swift
+++ Santander Exchange/Views/ButtonsView.swift
@@ -5,4 +5,27 @@
 //  Created by Josué Amorim on 08/10/23.
 //
 
-import Foundation
+import UIKit
+
+class ButtonsView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupUI()
+    }
+    
+    private func setupUI() {
+        backgroundColor = .red // Defina a cor de fundo desejada
+        layer.cornerRadius = 8
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.2
+        layer.shadowOffset = CGSize(width: 0, height: 2)
+        layer.shadowRadius = 4
+        layer.masksToBounds = false
+    }
+}
+
